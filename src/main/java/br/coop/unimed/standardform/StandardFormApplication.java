35b3c778d9e8f2595079cb6dc15775ccac596ba9
Maycon <maycob2@gmail.com>
2021-02-27 15:53:17 -0300

    Commit Inicial

---
+++ src/main/java/br/coop/unimed/standardform/StandardFormApplication.java
@@ -0,0 +1,13 @@
+package br.coop.unimed.standardform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardFormApplication.class, args);
+	}
+
+}
