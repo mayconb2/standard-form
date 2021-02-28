6beeaf24355d2c0bf7ab1a637cc3c105e17c5c04
Maycon <maycob2@gmail.com>
2021-02-28 19:03:48 -0300

    Adicionado primeira versão do Front

--- src/main/java/br/coop/unimed/standardform/controller/FormController.java
+++ src/main/java/br/coop/unimed/standardform/controller/FormController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/api/v1/form")
 public class FormController {
 
